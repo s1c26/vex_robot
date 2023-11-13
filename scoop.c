e29947e8e7c56150b83ffa7db92b64b3e11cc5ef
s1c26 <seancreed0126@gmail.com>
2023-11-13 13:40:35 -0500

    Added code to move servo slower

--- scoop.c
+++ scoop.c
@@ -8,22 +8,50 @@
 
 const int SCOOP_UP = 100;
 const int SCOOP_DOWN = -100;
-const int SCOOP_COOLDOWN = 50;
+const int SCOOP_MIDDLE = 0;
+const int SERVO_SPEED = 10;
+
+void setServoSpeed(int current_pos, int new_pos, int speed)
+{
+	if(new_pos == current_pos)
+	{
+		return;
+	}
+	else if(new_pos < current_pos)
+	{
+		for(int i = current_pos; i < new_pos; i++)
+		{
+			motor[scoop_servo] = i;
+			delay(1000 / speed);
+		}
+	}
+	else if(new_pos > current_pos)
+	{
+		for(int i = current_pos; i > new_pos; i--)
+		{
+			motor[scoop_servo] = i;
+			delay(1000 / speed);
+		}
+	}
+}
 
 task scoop()
 {
+	int current_position = 0;
 	for(;;)
 	{
 		// Set servo position
 		if(vexRT[Btn6U])
 		{
-			motor[shovel] = SCOOP_UP;
+			setServoSpeed(current_position, SCOOP_UP, SERVO_SPEED);
 		}
-		if(vexRT[Btn6D])
+		else if(vexRT[Btn6D])
 		{
-			motor[shovel] = SCOOP_DOWN;
+			setServoSpeed(current_position, SCOOP_DOWN, SERVO_SPEED);
 		}
-
-		delay(SCOOP_COOLDOWN);
+		else if(vexRT[Btn6D])
+		{
+			setServoSpeed(current_position, SCOOP_MIDDLE, SERVO_SPEED);
+		}
 	}
 }
